31fb256213dc8f735fc1775830a6967f31bc3b84
vyom10445 <vyom.2427030058@muj.manipal.edu>
2026-05-15 16:17:10 +0530

    train test split

--- logistic-regression/logistic-regression.py
+++ logistic-regression/logistic-regression.py
@@ -7,4 +7,14 @@ print(df.head())
 x=df.drop("target",axis=1).values
 y=df["target"].values
 
+#feature scaling
 x = x - x.mean(axis=0)/x.std(axis=0)
+
+#train test split
+split_index = int(0.8 * len(x))
+
+x_train = x[:split_index]  #take rows from beginning up to row {split_index}
+y_train = y[:split_index]  #take rows from {split_index} till end
+
+x_test = x[split_index:]
+y_test = y[split_index:]

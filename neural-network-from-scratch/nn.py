05b8182b23b83b3959d00b2037270519e214b38b
vyom10445 <vyom.2427030058@muj.manipal.edu>
2026-05-21 22:54:59 +0530

    forward method

--- neural-network-from-scratch/nn.py
+++ neural-network-from-scratch/nn.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-inputs = [[1,2,3,2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-weights =[[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases= [2,3,0.5]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
 
-layer_1_output = np.dot(inputs , np.array(weights).T)+biases
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
 
-biases2= [1, 2, -0.5]
-layer_2_output = np.dot(layer_1_output , np.array(weights2).T)+biases2
+layer1.forward(X)
+#print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)

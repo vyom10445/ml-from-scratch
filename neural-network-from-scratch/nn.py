d3338c758fcc8ce3c90d7e9804ea636d48b748c7
vyom10445 <vyom.2427030058@muj.manipal.edu>
2026-05-21 22:31:39 +0530

    add another layer

--- neural-network-from-scratch/nn.py
+++ neural-network-from-scratch/nn.py
@@ -12,4 +12,12 @@ biases= [2,3,0.5]
 
 
 layer_1_output = np.dot(inputs , np.array(weights).T)+biases
-print(layer_1_output)
+
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13]
+]
+
+biases2= [1, 2, -0.5]
+layer_2_output = np.dot(layer_1_output , np.array(weights2).T)+biases2
